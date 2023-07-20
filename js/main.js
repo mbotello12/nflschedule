2bdb16eb7f03d7ecbec8c9ea5e0988ce02d57edc
Matthew <matt.botello12@gmail.com>
2023-07-20 09:43:51 -0500

    Added files to nflschedule

---
+++ js/main.js
@@ -0,0 +1,271 @@
+document.querySelector('#bears').addEventListener('click', bearsGames)
+document.querySelector('#lions').addEventListener('click', lionsGames)
+document.querySelector('#packers').addEventListener('click', packersGames)
+document.querySelector('#vikings').addEventListener('click', vikingsGames)
+document.querySelector('#cowboys').addEventListener('click', cowboysGames)
+document.querySelector('#giants').addEventListener('click', giantsGames)
+document.querySelector('#eagles').addEventListener('click', eaglesGames)
+document.querySelector('#commanders').addEventListener('click', commandersGames)
+document.querySelector('#falcons').addEventListener('click', falconsGames)
+document.querySelector('#panthers').addEventListener('click', panthersGames)
+document.querySelector('#saints').addEventListener('click', saintsGames)
+document.querySelector('#bucs').addEventListener('click', bucsGames)
+document.querySelector('#cardinals').addEventListener('click', cardinalsGames)
+document.querySelector('#rams').addEventListener('click', ramsGames)
+document.querySelector('#niners').addEventListener('click', ninersGames)
+document.querySelector('#seahawks').addEventListener('click', hawksGames)
+document.querySelector('#ravens').addEventListener('click', ravensGames)
+document.querySelector('#bengals').addEventListener('click', bengalsGames)
+document.querySelector('#browns').addEventListener('click',brownsGames)
+document.querySelector('#steelers').addEventListener('click', steelersGames)
+document.querySelector('#bills').addEventListener('click', billsGames)
+document.querySelector('#dolphins').addEventListener('click', dolphinsGames)
+document.querySelector('#patriots').addEventListener('click', patriotsGames)
+document.querySelector('#jets').addEventListener('click', jetsGames)
+document.querySelector('#colts').addEventListener('click', coltsGames)
+document.querySelector('#texans').addEventListener('click', texansGames)
+document.querySelector('#jaguars').addEventListener('click', jaguarsGames)
+document.querySelector('#titans').addEventListener('click', titansGames)
+document.querySelector('#broncos').addEventListener('click', broncosGames)
+document.querySelector('#chiefs').addEventListener('click', chiefsGames)
+document.querySelector('#raiders').addEventListener('click', raidersGames)
+document.querySelector('#chargers').addEventListener('click', chargersGames)
+
+
+function bearsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/bears_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function lionsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/lions_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function packersGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/packers_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function vikingsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/vikings_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function cowboysGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/cowboys_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function giantsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/giants_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function eaglesGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/eagles_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function commandersGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/commanders_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function falconsGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/falcons_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function panthersGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/panthers_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function saintsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/saints_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function bucsGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/bucs_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function cardinalsGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/cards_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function ramsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/rams_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function ninersGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/niners_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function hawksGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/hawks_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function ravensGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/ravens_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function bengalsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/bengals_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function brownsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/browns_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function steelersGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/steelers_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function billsGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/bills_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function dolphinsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/dolphins_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function patriotsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/patriots_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function jetsGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/jets_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function broncosGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/broncos_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function chiefsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/chiefs_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function raidersGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/raiders_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function chargersGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/chargers_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function coltsGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/colts_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function texansGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/texans_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function jaguarsGames(){
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/jaguars_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
+
+function titansGames() {
+    document.querySelector('#schedule').style.backgroundImage = 'url(img/schedules/titans_schedule.png)'
+    document.querySelector('#schedule').style.backgroundPosition = 
+    'center'
+    document.querySelector('#schedule').style.backgroundSize = 'contain'
+    document.querySelector('#schedule').style.backgroundRepeat = 'no-repeat'
+}
